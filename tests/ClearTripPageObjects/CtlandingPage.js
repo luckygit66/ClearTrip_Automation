06ffaac5dd8c88a27b7c6f41e1b4cf63bea6d758
Lucky <lucky8463@gmail.com>
2025-09-22 17:45:34 +0530

    seperated the locators as per Page factory

--- tests/ClearTripPageObjects/CtlandingPage.js
+++ tests/ClearTripPageObjects/CtlandingPage.js
@@ -1,36 +1,46 @@
 class CtlandingPage {
     constructor(page) {
         this.page = page;
+        this.source="DEL";
+        this.destination = "BOM"
+        this.welcomePopupCloseButton= page.locator(".c-pointer.c-neutral-900[data-testid='closeIcon']");
+        this.tripType= page.locator(".fw-500.fs-4.ml-2");
+       this.selectTripOption= page.locator("//p[normalize-space()='Round trip']")
+       this.sourceRegion=page.locator("input[placeholder='Where from?']");
+       this.destinationRegion = page.locator("input[placeholder='Where to?']");
+       this.dateOnwards= page.locator("div[data-testid='dateSelectOnward']");
+       this.returnDate = page.locator("div[data-testid='dateSelectReturn']");
+       this.searchFlightsButton = page.locator("//h4[normalize-space()='Search flights']");
     }
 
 
     async MakeRoundTripSearch() {
         await this.page.waitForSelector(".c-pointer.c-neutral-900[data-testid='closeIcon']");
-        await this.page.locator(".c-pointer.c-neutral-900[data-testid='closeIcon']").click();
-        await this.page.locator(".fw-500.fs-4.ml-2").click();
-        await this.page.locator("//p[normalize-space()='Round trip']").click();
-        await this.page.locator("input[placeholder='Where from?']").clear();
-        await this.page.locator("input[placeholder='Where from?']").pressSequentially("DEL");
-        await this.page.locator(".airport-code:has-text('DEL')").click();
-        await this.page.locator("input[placeholder='Where to?']").clear();
-        await this.page.locator("input[placeholder='Where to?']").pressSequentially("BOM");
-        await this.page.locator(".airport-code:has-text('BOM')").click();
-        await this.page.locator("div[data-testid='dateSelectOnward']").click();
+        await this.welcomePopupCloseButton.click();
+        await this.tripType.click();
+        await this.selectTripOption.click();
+        await this.sourceRegion.clear();
+        await this.sourceRegion.pressSequentially(this.source);
+        await this.page.locator(`.airport-code:has-text('${this.source}')`).click();
+        await this.destinationRegion.clear();
+        await this.destinationRegion.pressSequentially(this.destination);
+        await this.page.locator(`.airport-code:has-text('${this.destination}')`).click();
+        await this.dateOnwards.click();
         //await this.page.pause();
         const startDate = this.generateFutureDate(3);
         console.log("target date is::", startDate);
         const startDateLocator = this.page.locator(`div[aria-label='${startDate}']`);
         await startDateLocator.click();
-        await this.page.locator("div[data-testid='dateSelectOnward']").click();
+        await this.dateOnwards.click();
         // await this.page.pause();
-        await this.page.locator("div[data-testid='dateSelectReturn']").click();
+        await this.returnDate.click();
         const endDate = this.generateFutureDate(8);
         console.log("target date is::", endDate)
         const endDateLocator = this.page.locator(`div[aria-label='${endDate}']`);
         await endDateLocator.click();
-        await this.page.locator("//h4[normalize-space()='Search flights']").click();
+        await this.searchFlightsButton.click();
         await this.page.title();
-        await this.page.waitForTimeout(7000);
+        await this.page.waitForTimeout(15000);
     }
 
 
